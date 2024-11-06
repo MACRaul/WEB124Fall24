e548be489eede7733f62a1be27609ce9a6179ca7
Raul Morales Alcazar <raulm3853@gmail.com>
2024-11-06 15:01:46 -0600

    upload ch 3 proj

---
+++ Chapter3Project/ch3proj.js
@@ -0,0 +1,31 @@
+/*Raul Morales Alcazar
+Chapter 3 Project
+06 NOV 2024*/
+
+// 1
+let fullName = "Raul Morales";
+console.log("Full Name: ", fullName);
+
+// 2
+let annualSalary = 50000;
+console.log("Annual Salary: ", annualSalary);
+
+// 3
+let isVeteran = false; 
+console.log("Is a veteran: ", isVeteran);
+
+// 4
+let friendsNames = ["Beth", "Kail", "Charlie"];
+console.log("Friends' Names:", friendsNames); 
+
+// 5
+let friendsSalaries = [45000, 60000, 55000];
+console.log("Friends' Salaries:", friendsSalaries); 
+
+// 6
+let anotherFriend = {
+    firstName: "David",
+    lastName: "Smith",
+    desiredAnnualSalary: 70000
+};
+console.log("Another Friend's Details:", anotherFriend);
